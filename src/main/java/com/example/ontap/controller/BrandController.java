b6171a5e6bba52b937dc7b4788d08aa229cae1db
duydep24022006 <memay26032006@gmail.com>
2026-05-04 10:00:13 +0700

    moi

--- src/main/java/com/example/ontap/controller/BrandController.java
+++ src/main/java/com/example/ontap/controller/BrandController.java
@@ -1,9 +1,28 @@
 package com.example.ontap.controller;
 
+import com.example.ontap.entity.Brand;
+import com.example.ontap.repository.IBrandRepository;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/brand")
 public class BrandController {
+
+    private final IBrandRepository brandRepository;
+
+    public BrandController(IBrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+    @GetMapping
+    public String listBrands(Model model) {
+        model.addAttribute("brands", brandRepository.findAll());
+        return "brand-list";
+    }
+
+
 }
